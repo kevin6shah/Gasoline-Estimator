17285aee65f0626d5816ed7a67d955571a5c4a0a
Kevin Shah <42818947+kevin6shah@users.noreply.github.com>
2019-04-02 21:30:52 -0400

    Removed API key

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 
-const kGoogleApiKey = "AIzaSyCPSnTtP8qqFwqjJeup1Ot7Qby_-MVar50";
+const kGoogleApiKey = /*<KEY>*/;
 
 void main() => runApp(
       MaterialApp(home: MyApp()),
